26d4fddf911b4fed8a10a54c1553db152a13728c
Amresh Yadav <amreshky998@gmail.com>
2025-10-07 22:41:11 +0530

    mobile link fixed

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL});
+console.log("console.log(import.meta.env.VITE_API_URL);", import.meta.env.VITE_API_URL);
 
 export async function fetchExpenses() {
   const { data } = await api.get('/expenses');
